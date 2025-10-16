0b16409387d6592ceeffdcb4b6d5e999f27faea1
Sarthak <ncssarthak@gmail.com>
2025-10-16 14:22:53 +0530

    CLG WEB DEV

---
+++ ES6/dom.js
@@ -0,0 +1,6 @@
+// const div=document.createElement("div");
+// const h1=document.createElement("h1");
+// h1.innerText="hi sarthak";
+// const id=document.getElementById("root");
+// div.appendChild(h1);
+// id.appendChild(div);
